6720da2500be5f70126e442ee2ba250c2b24984a
farshid92 <farshidcheraghchian@gmail.com>
2025-07-02 15:47:53 +0300

    update segmentation notebook with augmented data

--- backend/inference.py
+++ backend/inference.py
@@ -14,7 +14,7 @@ def preprocess_image(image_path, target_size=(128, 128)):
     img_array = np.array(img) / 255.0
     return np.expand_dims(img_array, axis=0)
 
-def postprocess_mask(mask, save_path="predicted_mask.png", threshold=0.15):
+def postprocess_mask(mask, save_path="predicted_mask.png", threshold=0.1):
     print("prediction stast= min:", mask.min(), "max", mask.max(), "mean", mask.mean())
 
     binarized = (mask[0].squeeze() > threshold).astype(np.uint8)* 255
